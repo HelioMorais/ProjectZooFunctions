9f736ef1c364091a754c19ca9bfb6aef02b07b34
Helio Morais <heliogov20@gmail.com>
2023-03-09 18:37:47 -0300

    Requisito 5, VAMOS COM TUDO :rocket:

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
-};
-
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const specie = data.species.find((element) => element.name === animal.species);
+  const sex = animal.sex ? specie.residents
+    .filter((element) => element.sex === animal.sex) : specie.residents;
+  return sex.reduce((acc) => acc + 1, 0);
+}
+console.log(countAnimals());
+console.log(countAnimals({ species: 'penguins' }));
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
