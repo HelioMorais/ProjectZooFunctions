ae313283645d9c8e0c360212e2fd93b5d0afcde3
Helio Morais <heliogov20@gmail.com>
2023-03-09 15:35:21 -0300

    Requisito 2, VAMOS COM TUDO :rocket:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
+  const species = data.species.find((element) => element.name === animal);
 
+  if (!species) {
+    return false;
+  }
+
+  return species.residents.every((element) => element.age >= age);
+};
+console.log(getAnimalsOlderThan('bears', 4));
 module.exports = getAnimalsOlderThan;
